45cf366e91f9cd2604a2d1506bff36f4556e0d8a
Brendan Long <self@brendanlong.com>
2015-04-15 15:53:06 -0500

    Add more ISOBMFF tests.

--- tests/isobmff.c
+++ tests/isobmff.c
@@ -27,6 +27,7 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <check.h>
+#include <glib.h>
 
 #include "isobmff.h"
 #include "test_common.h"
@@ -146,7 +147,132 @@ START_TEST(test_read_representation_index_with_subsegment_index)
 
     free_boxes(boxes, boxes_len);
 END_TEST
-#include <stdio.h>
+
+START_TEST(test_read_boxes_from_stream)
+    GError* gerror = NULL;
+    char* contents;
+    gsize length;
+    ck_assert(g_file_get_contents("tests/subsegment-example.six", &contents, &length, &gerror));
+    ck_assert_ptr_eq(gerror, NULL);
+
+    bitreader_t* b = bitreader_new((uint8_t*)contents, length);
+    size_t boxes_len;
+    int error = 0;
+    box_t** boxes = read_boxes_from_stream(b, &boxes_len, &error);
+
+    ck_assert(!error);
+    ck_assert_ptr_ne(boxes, NULL);
+    ck_assert_uint_eq(boxes_len, 54);
+
+    box_t* box = boxes[0];
+    char type[5] = {0};
+    ck_assert_ptr_ne(box, NULL);
+    uint32_to_string(type, box->type);
+    ck_assert_str_eq(type, "styp");
+    ck_assert_uint_eq(box->size, 24);
+
+    styp_t* styp = (styp_t*)box;
+    char major_brand[5] = {0};
+    uint32_to_string(major_brand, styp->major_brand);
+    ck_assert_str_eq(major_brand, "risx");
+    ck_assert_uint_eq(styp->minor_version, 0);
+    ck_assert_uint_eq(styp->num_compatible_brands, 2);
+    char* expected_compatible_brands[2] = {"risx", "ssss"};
+    for (size_t i = 0; i < styp->num_compatible_brands; ++i) {
+        char compatible_brand[5] = {0};
+        uint32_to_string(compatible_brand, styp->compatible_brands[i]);
+        ck_assert_str_eq(compatible_brand, expected_compatible_brands[i]);
+    }
+
+    box = boxes[1];
+    ck_assert_ptr_ne(box, NULL);
+    uint32_to_string(type, box->type);
+    ck_assert_str_eq(type, "sidx");
+    ck_assert_uint_eq(box->size, 344);
+
+    sidx_t* sidx = (sidx_t*)box;
+    ck_assert_uint_eq(sidx->version, 0);
+    ck_assert_uint_eq(sidx->flags, 0);
+    ck_assert_uint_eq(sidx->reference_id, 256);
+    ck_assert_uint_eq(sidx->timescale, 90000);
+    ck_assert_uint_eq(sidx->earliest_presentation_time, 0);
+    ck_assert_uint_eq(sidx->first_offset, 0);
+    ck_assert_uint_eq(sidx->reference_count, 26);
+    {
+        uint32_t expected_referenced_size[26] = {948, 944, 944, 944, 944, 944, 944, 944, 944, 944, 944, 944, 944, 944,
+                944, 944, 944, 944, 944, 944, 944, 944, 944, 944, 944, 696};
+        for (size_t i = 0; i < sidx->reference_count; ++i) {
+            sidx_reference_t* reference = &sidx->references[i];
+            ck_assert_uint_eq(reference->reference_type, 1);
+            ck_assert_uint_eq(reference->referenced_size, expected_referenced_size[i]);
+            ck_assert_uint_eq(reference->subsegment_duration, (i == sidx->reference_count - 1) ? 645000 : 900000);
+            ck_assert(!reference->starts_with_sap);
+            ck_assert_uint_eq(reference->sap_type, 0);
+            ck_assert_uint_eq(reference->sap_delta_time, 0);
+        }
+    }
+
+    for (size_t box_i = 0; box_i < 26; ++box_i) {
+        box = boxes[box_i * 2 + 2];
+        ck_assert_ptr_ne(box, NULL);
+        uint32_to_string(type, box->type);
+        ck_assert_str_eq(type, "sidx");
+        ck_assert_uint_eq(box->size, (box_i == 25) ? 80 : 92);
+
+        sidx = (sidx_t*)box;
+        ck_assert_uint_eq(sidx->version, 0);
+        ck_assert_uint_eq(sidx->flags, 0);
+        ck_assert_uint_eq(sidx->reference_id, 256);
+        ck_assert_uint_eq(sidx->timescale, 90000);
+        ck_assert_uint_eq(sidx->earliest_presentation_time, 6000 + 900000 * box_i);
+        ck_assert_uint_eq(sidx->first_offset, 376);
+        ck_assert_uint_eq(sidx->reference_count, (box_i == 25) ? 4 : 5);
+
+        uint32_t expected_referenced_size[26][5] = {
+                {144948, 179164, 225788, 228796, 220712},
+                {207176, 205108, 219960, 214320, 226916},
+                {224096, 162808, 173524, 191384, 185556},
+                {214884, 222968, 204544, 209996, 221652},
+                {214320, 214132, 219584, 200220, 185744},
+                {194580, 209432, 216200, 225788, 194392},
+                {194204, 203604, 218832, 214884, 239888},
+                {180480, 218268, 218080, 224660, 215072},
+                {203416, 203416, 222216, 221840, 220148},
+                {215448, 215072, 204732, 187060, 205296},
+                {209996, 214132, 223720, 228420, 227104},
+                {209432, 219772, 230112, 206612, 189880},
+                {192136, 229736, 179352, 203416, 238948},
+                {173712, 167508, 231428, 219772, 228420},
+                {221840, 221276, 227104, 221464, 222028},
+                {216576, 199092, 213004, 199656, 211500},
+                {212252, 220524, 207176, 221652, 240076},
+                {221840, 190632, 177284, 193828, 212440},
+                {218644, 214696, 203416, 209808, 221088},
+                {228044, 202852, 195708, 203792, 206612},
+                {203792, 205484, 225788, 216576, 227856},
+                {222592, 225412, 222780, 221840, 218080},
+                {216952, 222592, 247220, 134044, 131036},
+                {58844, 59220, 62416, 59972, 190256},
+                {58280, 57152, 60536, 57528, 157920},
+                {41736, 17860, 18800, 38352}
+        };
+
+        for (size_t i = 0; i < sidx->reference_count; ++i) {
+            sidx_reference_t* reference = &sidx->references[i];
+            ck_assert_uint_eq(reference->reference_type, 0);
+            ck_assert_uint_eq(reference->referenced_size, expected_referenced_size[box_i][i]);
+            ck_assert_uint_eq(reference->subsegment_duration, (box_i == 25 && i == sidx->reference_count - 1) ? 105000 : 180000);
+            ck_assert(reference->starts_with_sap);
+            ck_assert_uint_eq(reference->sap_type, 1);
+            ck_assert_uint_eq(reference->sap_delta_time, 0);
+        }
+    }
+
+    free_boxes(boxes, boxes_len);
+    bitreader_free(b);
+    g_free(contents);
+END_TEST
+
 START_TEST(test_read_representation_index_with_pcrb)
     int error = 0;
     size_t boxes_len;
@@ -219,6 +345,73 @@ START_TEST(test_read_representation_index_with_pcrb)
     free_boxes(boxes, boxes_len);
 END_TEST
 
+START_TEST(test_read_styp)
+    uint8_t bytes[] = {0, 0, 0, 24, 's', 't', 'y', 'p', 'a', 'b', 'c', 'd', 1, 'f', 'g', 'h', 'r', 'i', 's', 'x',
+            'r', 's', 't', 'l'};
+
+    bitreader_t* b = bitreader_new(bytes, sizeof(bytes));
+    size_t boxes_len;
+    int error = 0;
+    box_t** boxes = read_boxes_from_stream(b, &boxes_len, &error);
+
+    ck_assert(!error);
+    ck_assert_ptr_ne(boxes, NULL);
+    ck_assert_uint_eq(boxes_len, 1);
+
+    box_t* box = boxes[0];
+    char type[5] = {0};
+    ck_assert_ptr_ne(box, NULL);
+    uint32_to_string(type, box->type);
+    ck_assert_str_eq(type, "styp");
+    ck_assert_uint_eq(box->size, 24);
+
+    styp_t* styp = (styp_t*)box;
+    char major_brand[5] = {0};
+    uint32_to_string(major_brand, styp->major_brand);
+    ck_assert_str_eq(major_brand, "abcd");
+    ck_assert_uint_eq(styp->minor_version, 23488360);
+    ck_assert_uint_eq(styp->num_compatible_brands, 2);
+    char* expected_compatible_brands[2] = {"risx", "rstl"};
+    for (size_t i = 0; i < styp->num_compatible_brands; ++i) {
+        char compatible_brand[5] = {0};
+        uint32_to_string(compatible_brand, styp->compatible_brands[i]);
+        ck_assert_str_eq(compatible_brand, expected_compatible_brands[i]);
+    }
+
+    free_boxes(boxes, boxes_len);
+    bitreader_free(b);
+END_TEST
+
+START_TEST(test_read_styp_no_compatible_brands)
+    uint8_t bytes[] = {0, 0, 0, 16, 's', 't', 'y', 'p', 'a', 'b', 'c', 'd', 1, 'f', 'g', 'h'};
+
+    bitreader_t* b = bitreader_new(bytes, sizeof(bytes));
+    size_t boxes_len;
+    int error = 0;
+    box_t** boxes = read_boxes_from_stream(b, &boxes_len, &error);
+
+    ck_assert(!error);
+    ck_assert_ptr_ne(boxes, NULL);
+    ck_assert_uint_eq(boxes_len, 1);
+
+    box_t* box = boxes[0];
+    char type[5] = {0};
+    ck_assert_ptr_ne(box, NULL);
+    uint32_to_string(type, box->type);
+    ck_assert_str_eq(type, "styp");
+    ck_assert_uint_eq(box->size, 16);
+
+    styp_t* styp = (styp_t*)box;
+    char major_brand[5] = {0};
+    uint32_to_string(major_brand, styp->major_brand);
+    ck_assert_str_eq(major_brand, "abcd");
+    ck_assert_uint_eq(styp->minor_version, 23488360);
+    ck_assert_uint_eq(styp->num_compatible_brands, 0);
+
+    free_boxes(boxes, boxes_len);
+    bitreader_free(b);
+END_TEST
+
 Suite *suite(void)
 {
     Suite *s;
@@ -230,7 +423,10 @@ Suite *suite(void)
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_read_representation_index_with_subsegment_index);
+    tcase_add_test(tc_core, test_read_boxes_from_stream);
     tcase_add_test(tc_core, test_read_representation_index_with_pcrb);
+    tcase_add_test(tc_core, test_read_styp);
+    tcase_add_test(tc_core, test_read_styp_no_compatible_brands);
 
     suite_add_tcase(s, tc_core);
 
